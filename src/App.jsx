fd3ceb35707ed52e73a905fbf68efa6e34b4d197
Chyisiang <chyisiang8@gmail.com>
2023-07-21 19:03:26 +0800

    product cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import ProductCard from "./components/ProductCard";
 
-function App() {
-
+export default function App() {
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <ProductCard></ProductCard>
     </>
   )
 }
-
-export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ProductCard = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // Fetch products from the API using axios
+        axios.get('https://fakestoreapi.com/products')
+            .then((response) => {
+                setProducts(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching products:', error);
+            });
+    }, []);
+
+    const addToCart = (product) => {
+        // Implement logic to add the product to the cart
+        // You can use state or Redux to manage the cart items
+        console.log('Added to cart:', product);
+    };
+
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            {products.map((product) => (
+                <div key={product.id} className="border p-4">
+                    <img src={product.image} alt={product.title} className="mx-auto mb-2" style={{ width: '150px' }} />
+                    <h2 className="text-xl font-semibold mb-2">{product.title}</h2>
+                    <p className="text-gray-600">${product.price}</p>
+                    <div className="flex items-center mt-2">
+                        <button
+                            className="bg-blue-500 text-white px-2 py-1 rounded"
+                            onClick={() => addToCart(product)}
+                        >
+                            Add To Cart
+                        </button>
+                        {/* Implement quantity input field and increment/decrement buttons here */}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProductCard;
