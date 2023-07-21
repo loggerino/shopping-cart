a873e98e6560ddcd34995e931d5cb0bd4f036749
Chyisiang <chyisiang8@gmail.com>
2023-07-21 21:49:49 +0800

    fixed total amount

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,20 +1,18 @@
+import React from 'react';
+import CartItem from './CartItem';
+
 function ShoppingCart({ cartItems }) {
     const totalAmount = cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
 
     return (
-        <div
-            className="bg-white p-4 rounded shadow-lg absolute top-14 right-4 z-10"
-            style={{ minWidth: '20rem' }}
-        >
+        <div className="bg-white p-4 rounded shadow-lg absolute top-14 right-4 z-10" style={{ minWidth: '20rem' }}>
             <h2 className="text-xl font-semibold mb-4">Shopping Cart</h2>
             {cartItems.map((cartItem) => (
-                <div key={cartItem.product.id} className="mb-2">
-                    <p>{cartItem.product.title} - Quantity: {cartItem.quantity}</p>
-                </div>
+                <CartItem key={cartItem.product.id} cartItem={cartItem} />
             ))}
+            <p className="text-xl font-semibold mt-4">Total Amount: ${totalAmount.toFixed(2)}</p>
         </div>
     );
-};
+}
 
-
-export default ShoppingCart
+export default ShoppingCart;
