1f7f170cfbcbd5db0d627335cde2ab39e0c32a10
Chyisiang <chyisiang8@gmail.com>
2023-07-21 20:27:03 +0800

    increment decrement

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ProductCard({ product, addToCart }) {
+    const [quantity, setQuantity] = useState(1);
     const { title, image, price } = product;
+
+    const handleIncrement = () => {
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    };
+
+    const handleDecrement = () => {
+        if (quantity > 1) {
+            setQuantity((prevQuantity) => prevQuantity - 1);
+        }
+    };
 
     return (
         <div className="border p-4">
@@ -9,12 +20,23 @@ function ProductCard({ product, addToCart }) {
             <h2 className="text-xl font-semibold mb-2">{title}</h2>
             <p className="text-gray-600">${price}</p>
             <div className="flex items-center mt-2">
-                <button
-                    className="bg-blue-500 text-white px-2 py-1 rounded"
-                    onClick={() => addToCart(product)}
-                >
+                <button className="bg-blue-500 text-white px-2 py-1 rounded" onClick={() => addToCart(product, quantity)}>
                     Add To Cart
                 </button>
+                <div className="ml-4 flex items-center border rounded">
+                    <button className="px-2" onClick={handleDecrement}>
+                        -
+                    </button>
+                    <input
+                        type="number"
+                        value={quantity}
+                        onChange={(e) => setQuantity(parseInt(e.target.value))}
+                        className="w-12 text-center border-r border-l"
+                    />
+                    <button className="px-2" onClick={handleIncrement}>
+                        +
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -4,6 +4,7 @@ import ProductCard from './ProductCard';
 
 function ShoppingPage() {
     const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
     useEffect(() => {
         axios.get('https://fakestoreapi.com/products')
@@ -15,17 +16,45 @@ function ShoppingPage() {
             });
     }, []);
 
-    const addToCart = (product) => {
-        console.log('Added to cart:', product);
+    const addToCart = (product, quantity) => {
+        // Check if the product is already in the cart
+        const existingCartItem = cartItems.find((item) => item.product.id === product.id);
+
+        if (existingCartItem) {
+            // Update the quantity if the product is already in the cart
+            const updatedCartItems = cartItems.map((item) =>
+                item.product.id === product.id ? { ...item, quantity } : item
+            );
+            setCartItems(updatedCartItems);
+        } else {
+            // Add the product to the cart if it's not already there
+            setCartItems([...cartItems, { product, quantity }]);
+        }
+        console.log('Added to cart:', product.title, '-', quantity, 'quantity');
     };
 
+    // Calculate the total amount
+    const totalAmount = cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
+
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} addToCart={addToCart} />
-            ))}
+        <div>
+            <div className="grid grid-cols-3 gap-4">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} addToCart={addToCart} />
+                ))}
+            </div>
+            <div>
+                <h2 className="text-2xl font-semibold mt-4">Shopping Cart</h2>
+                {cartItems.map((cartItem) => (
+                    <div key={cartItem.product.id}>
+                        <p>{cartItem.product.title} - Quantity: {cartItem.quantity}</p>
+                    </div>
+                ))}
+                <p className="text-xl font-semibold mt-4">Total Amount: ${totalAmount.toFixed(2)}</p>
+            </div>
         </div>
     );
 };
+
 
 export default ShoppingPage;
